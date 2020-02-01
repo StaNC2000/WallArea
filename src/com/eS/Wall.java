f53466d01b301dbf45ad2ffe9e54fea55bf0aad3
es <StaNC2000>
2020-02-01 19:23:26 +0100

    Solution added and tested

--- src/com/eS/Wall.java
+++ src/com/eS/Wall.java
@@ -5,5 +5,57 @@ public class Wall {
     private double width;
     private double height;
 
+    public Wall(){
+        this.height = getHeight();
+        this.width = getWidth();
+    }
+
+    public Wall(double width, double height){
+        if (width < 0) {
+            this.width = 0;
+        }
+        else {
+            this.width = width;
+        }
+        if (height < 0) {
+            this.height = 0;
+        }
+        else {
+            this.height = height;
+        }
+    }
+
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setWidth(double width) {
+        if (width < 0){
+            this.width = 0;
+        }
+        else {
+            this.width = width;
+        }
+    }
+
+    public void setHeight(double height) {
+        if (height < 0){
+            this.height = 0;
+        }
+        else {
+            this.height = height;
+        }
+    }
+
+    public double getArea(){
+        return getHeight() * getWidth();
+
+    }
+
 
 }
